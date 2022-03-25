cf5f2f9d8db8df9df08d4d3091ac20c19ddd2e41
HongNgoc Hospital <hongngoc@hongngoc.vn>
2022-03-25 09:05:47 +0700

    dev intro

--- lib/controllers/intro_controller.dart
+++ lib/controllers/intro_controller.dart
@@ -1,12 +1,21 @@
+import 'package:flutter_stater/adapters/repository/home/user_repository.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class IntroController extends GetxController {
 
-  IntroController() {
+  final GetStorage box;
+  final UserRepository userRepository;
+
+  IntroController({
+    required this.box,
+    required this.userRepository,
+  }) {
     init();
   }
 
   void init() {
-
   }
+
+
 }
--- lib/models/settings/bed_model.g.dart
+++ lib/models/settings/bed_model.g.dart
@@ -7,12 +7,12 @@ part of 'bed_model.dart';
 // **************************************************************************
 
 _$_BedModel _$$_BedModelFromJson(Map<String, dynamic> json) => _$_BedModel(
-      bedId: json['bedId'] as int?,
-      bedName: json['bedName'] as String?,
+      bedId: json['bed_id'] as int?,
+      bedName: json['bed_name'] as String?,
     );
 
 Map<String, dynamic> _$$_BedModelToJson(_$_BedModel instance) =>
     <String, dynamic>{
-      'bedId': instance.bedId,
-      'bedName': instance.bedName,
+      'bed_id': instance.bedId,
+      'bed_name': instance.bedName,
     };
