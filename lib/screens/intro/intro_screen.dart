ec2994bd156a0ca92e7fe344c40a4cd3c21bb845
thangcu <kenishcu@gmail.com>
2022-07-05 10:24:12 +0700

    fix bugs

--- lib/screens/intro/intro_screen.dart
+++ lib/screens/intro/intro_screen.dart
@@ -2,14 +2,143 @@ import 'package:flutter_stater/controllers/index.dart';
 import 'package:flutter_stater/layout/setting_layout.dart';
 import 'package:flutter_stater/routes/app_pages.dart';
 import 'package:flutter_stater/screens/intro/widgets/youtube_player.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../../controllers/setting_controller.dart';
 
-class IntroScreen extends GetView<IntroController> {
+class IntroScreen extends StatefulWidget {
 
   const IntroScreen({Key? key}) : super(key: key);
+
+  @override
+  IntroScreenState createState() => IntroScreenState();
+}
+
+class IntroScreenState extends State<IntroScreen> {
+
+  late FToast fToast;
+
+  late String _timeString;
+
+  IntroController controller = Get.find<IntroController>();
+
+  @override
+  void initState() {
+    super.initState();
+    fToast = FToast();
+    fToast.init(context);
+    _timeString = _formatDateTime(DateTime.now());
+  }
+
+  String _formatDateTime(DateTime dateTime) {
+    return DateFormat('hh:mm').format(dateTime);
+  }
+
+  _showToastSuccess() {
+    Widget toast = Container(
+      height: 80,
+      width: 280,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        color: Theme.of(context).colorScheme.secondaryContainer,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Expanded(
+            flex: 1,
+            child: Icon(Icons.bed_outlined, size: 28, color: Theme.of(context).colorScheme.secondary),
+          ),
+          Expanded(
+              flex: 3,
+              child: SizedBox(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    SizedBox(
+                      height: 40,
+                      child: Text("Lấy thông tin bệnh nhân thành công", style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14
+                      )),
+                    ),
+                  ],
+                ),
+              )
+          ),
+          Expanded(
+            flex: 1,
+            child: Text(_timeString),
+          )
+          ,
+        ],
+      ),
+    );
+
+    fToast.showToast(
+      child: toast,
+      gravity: ToastGravity.TOP_LEFT,
+      toastDuration: const Duration(seconds: 4),
+    );
+  }
+
+  _showToastError() {
+
+    Widget toast = Container(
+      height: 80,
+      width: 280,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        color: Colors.red,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Expanded(
+            flex: 1,
+            child: Icon(Icons.bed_outlined, size: 28, color: Colors.white),
+          ),
+          Expanded(
+              flex: 3,
+              child: SizedBox(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    SizedBox(
+                      height: 40,
+                      child: Text("Lấy thông tin bệnh nhân thất bại", style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                          color: Colors.white
+                      )),
+                    ),
+                  ],
+                ),
+              )
+          ),
+          Expanded(
+            flex: 1,
+            child: Text(_timeString, style: const TextStyle(
+                color: Colors.white
+            ),),
+          )
+          ,
+        ],
+      ),
+    );
+
+
+    fToast.showToast(
+      child: toast,
+      gravity: ToastGravity.TOP_LEFT,
+      toastDuration: const Duration(seconds: 4),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -72,7 +201,10 @@ class IntroScreen extends GetView<IntroController> {
                           onPressed: () async {
                             bool res = await controller.getUserInfo();
                             if(res) {
+                              _showToastSuccess();
                               Get.offAndToNamed(Routes.HOME);
+                            } else {
+                              _showToastError();
                             }
                           },
                           child: Text("Tiếp theo", style: TextStyle(
