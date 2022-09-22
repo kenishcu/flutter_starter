151d891e3d704a8a46e3feef586284fe2ffe0a8a
hoaint <hoaint116@gmail.com>
2022-09-22 10:39:11 +0700

    fix bug json category_id -> _id

--- lib/models/restaurant/sub_category_model.g.dart
+++ lib/models/restaurant/sub_category_model.g.dart
@@ -8,7 +8,7 @@ part of 'sub_category_model.dart';
 
 _$_SubCategoryModel _$$_SubCategoryModelFromJson(Map<String, dynamic> json) =>
     _$_SubCategoryModel(
-      categoryId: json['category_id'] as String?,
+      categoryId: json['_id'] as String?,
       categoryCode: json['category_code'] as String?,
       categoryName: json['category_name'] as String?,
       lang: json['lang'] as Map<String, dynamic>?,
@@ -16,7 +16,7 @@ _$_SubCategoryModel _$$_SubCategoryModelFromJson(Map<String, dynamic> json) =>
 
 Map<String, dynamic> _$$_SubCategoryModelToJson(_$_SubCategoryModel instance) =>
     <String, dynamic>{
-      'category_id': instance.categoryId,
+      '_id': instance.categoryId,
       'category_code': instance.categoryCode,
       'category_name': instance.categoryName,
       'lang': instance.lang,
